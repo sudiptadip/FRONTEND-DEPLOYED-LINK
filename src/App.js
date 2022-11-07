f121445f5a2f1f21db830281d998061608e89402
sudiptadip <sudiptabhattacharjee000@gmail.com>
2022-11-07 16:48:49 +0530

    final commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import AlllRoute from './components/AlllRoute';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+   <div>
+      {/* <Navbar/> */}
+      <AlllRoute />
+   </div>
+  );
+}
+
+export default App;
