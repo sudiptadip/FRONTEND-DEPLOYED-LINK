ac75a6393aa21ce97057e2e7db4458a11df33bdb
sudiptadip <sudiptabhattacharjee000@gmail.com>
2022-11-07 17:02:12 +0530

    last

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
-import AlllRoute from './components/AlllRoute';
-import Navbar from './components/Navbar';
+
 
 function App() {
   return (
    <div>
-      {/* <Navbar/> */}
-      <AlllRoute />
+      ok
    </div>
   );
 }
